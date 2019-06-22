a571e2552477fde279bc7ae9cf13de9f613b02cc
Sandeep Kumar <kumar-sand@HCL.COM>
2019-06-22 21:26:08 +0530

    my first commit

---
+++ spring-rest-full/src/main/java/org/baeldung/persistence/IOperations.java
@@ -0,0 +1,26 @@
+package org.baeldung.persistence;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface IOperations<T extends Serializable> {
+
+    // read - one
+
+    T findOne(final long id);
+
+    // read - all
+
+    List<T> findAll();
+
+    // write
+
+    T create(final T entity);
+
+    T update(final T entity);
+
+    void delete(final T entity);
+
+    void deleteById(final long entityId);
+
+}
